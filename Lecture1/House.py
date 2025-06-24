5f5db39028003096a13aa040b17631eedc939354
Aurko264 <aurko264@gmail.com>
2025-06-25 02:52:08 +0530

    new file

---
+++ Lecture1/House.py
@@ -0,0 +1,34 @@
+name = input("What's your name? ")
+
+# One way 
+
+"""if name == "Harry" or name == "Hermione" or name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else: 
+    print("Who?")"""
+
+# Another way
+
+"""match name:
+    case "Harry":
+        print("Gryffindor")
+    case "Hermione":
+        print("Gryffindor")
+    case "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")"""
+
+# Better way
+
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
