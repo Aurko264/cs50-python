05769f6f17160da77aae03ac79dc17502cb5155d
Aurko264 <aurko264@gmail.com>
2025-06-27 00:26:52 +0530

    new file

---
+++ Lecture1/PROBLEM SET 1/deep.py
@@ -0,0 +1,8 @@
+answer = input("what is the Answer to the Great Question of life, the Universe, and Everything?").strip().lower()
+
+if answer == "42" or answer == "forty two" or answer == "forty-two":
+    print("Yes")
+else:
+    print("No")
+
+
